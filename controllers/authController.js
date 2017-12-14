47a3aa593e1e8d96725b72dc41b3fb19c5c77267
uncleoptimus <uncle.optimus@gmail.com>
2017-12-13 20:04:33 -0800

    Add login/logout auth flow, User schema

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed login!',
+  successRedirect: '/',
+  successFlash: 'You are logged in.'
+});
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out!');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if User is authenticated
+  if (req.isAuthenticated()) {
+    next();
+  }
+
+  req.flash('error', 'Please login first to continue.');
+  res.redirect('/login');
+}
